b2073819cabdf6237f18f860302983cd7a9af7a4
Ammar Jussa <ammar26497@gmail.com>
2022-06-29 13:22:55 +0500

    Rightbar updated

--- components/Rightbar/index.tsx
+++ components/Rightbar/index.tsx
@@ -5,29 +5,9 @@ interface Props {}
 const Rightbar: React.FC<Props> = () => {
   const trends = [
     {
-      img: "spaceshooter.jpeg",
-      text: "Learn how to build a Web3 FPS game using unity...",
-      link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-space-fps-game/",
-    },
-    {
-      img: "netflix.jpeg",
-      text: "The fisrt Moralis Project! Let's Netflix and chill...",
-      link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-netflix-clone/",
-    },
-    {
-      img: "academy.png",
-      text: "Master DeFi in 2022. Start  at the Moralis Academy...",
-      link: "https://academy.moralis.io/courses/defi-101",
-    },
-    {
-      img: "js.png",
-      text: "Become a Web3 Developer with just simple JS...",
-      link: "https://academy.moralis.io/all-courses",
-    },
-    {
-      img: "youtube.png",
-      text: "Best youtube channel to learn about Web3...",
-      link: "https://www.youtube.com/channel/UCgWS9Q3P5AxCWyQLT2kQhBw",
+      img: "portfolio-web.png",
+      text: "Check out my portfolio site",
+      link: "https://ammarjussa.com/",
     },
   ];
   return (
